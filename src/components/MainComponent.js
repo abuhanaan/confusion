24e5a92e26c309f11d8ae9c9cc37c651e1dad711
Qomorudeen Mustopha <adewunmi99@gmail.com>
2022-08-19 19:37:29 +0100

    SPA part1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,14 @@
 import { Component } from 'react';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom'
 
 
@@ -12,14 +16,20 @@ class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dishes: DISHES
+      dishes: DISHES,
+      comments: COMMENTS,
+      leaders: LEADERS,
+      promotions: PROMOTIONS
     };
   }
 
   render(){
     const HomePage = () => {
         return (
-            <Home />
+            <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                  promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                  leader={this.state.leaders.filter((lead) => lead.featured)[0]}
+            />
         )
     }
     return (
@@ -30,6 +40,7 @@ class Main extends Component {
             <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
             {/* Emphasis on how data is being passed into the Menu component inside the Route
             using an arrow function in the above line of code */}
+            <Route exact path="/contactus" component={Contact} />
             <Redirect to="/home" />
         </Switch>
         <Footer />
