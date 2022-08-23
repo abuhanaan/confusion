28522af988cfe43a1ae64cd1c308f1fd0fd8558f
Qomorudeen Mustopha <adewunmi99@gmail.com>
2022-08-23 02:15:47 +0100

    About Component Implemented and Assignment2 completed

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { Switch, Route, Redirect } from 'react-router-dom'
 
 
@@ -50,6 +51,7 @@ class Main extends Component {
             using an arrow function in the above line of code */}
             <Route path="/menu/:dishId" component={DishWithId} />
             <Route exact path="/contactus" component={Contact} />
+            <Route path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
             <Redirect to="/home" />
         </Switch>
         <Footer />
