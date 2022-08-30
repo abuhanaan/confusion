1f4ab4f45f5413b37440c7021c32bc419116d4df
Qomorudeen Mustopha <adewunmi99@gmail.com>
2022-08-30 16:05:35 +0100

    React Redux Form Revisited

---
+++ src/redux/forms.js
@@ -0,0 +1,10 @@
+// this would enable us to reset the form back to its initial state after submiting it
+export const InitialFeedback = {
+    firstname: '',
+    lastname: '',
+    telnum: '',
+    email: '',
+    agree: false,
+    contactType: 'Tel.',
+    message: ''
+}
